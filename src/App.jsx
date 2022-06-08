2ae2898b6c9d9be4db4ead27683ad13e061923cb
Spectramus <64159706+Ahljenn@users.noreply.github.com>
2022-06-07 18:11:02 -0700

    Separated css files

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,14 @@
 import './styles/App.css';
 import SplitDisplay from './components/SplitDisplay';
 
-
-
 export default function App() {
   return (
     <>
+     <hr/>
       <header>
         <h1 className="title">Workout Planner</h1>
       </header>
-
-    <SplitDisplay />
-{/* 
-      <Marquee pauseOnClick={true}>
-        {
-        splits.map((split, index) => {
-          if (index % 2 === 0) return <h1 className="rolling-text">{split}</h1>
-          else return <h1 className="rolling-text-odd">{split}</h1>
-        })}
-      </Marquee> */}
-
+      <SplitDisplay />
     </>
   );
 }
--- src/components/SplitDisplay.jsx
+++ src/components/SplitDisplay.jsx
@@ -12,8 +12,7 @@ const splitsInfo = [
     { "name" : "5-Day Split",
       "info" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
     },
-  ];
-
+];
 
 export default function SplitDisplay() {
   return (
