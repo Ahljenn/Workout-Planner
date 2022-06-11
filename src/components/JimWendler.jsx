f2dc5ed9cb9ecdc7a4424149739283fb151f7430
Spectramus <64159706+Ahljenn@users.noreply.github.com>
2022-06-11 12:07:59 -0700

    Fixed reset

--- src/components/JimWendler.jsx
+++ src/components/JimWendler.jsx
@@ -1,44 +1,62 @@
 import React, { useState, useEffect } from 'react'
 
 export default function JimWendler() {
-    const [weightState, setWeightState] = useState(0);
-    const [repState, setRepState] = useState(0);
+    const [weightState, setWeightState] = useState("");
+    const [repState, setRepState] = useState("");
     const [estimate, setEstimate] = useState([]);
+    let temp = []; //Used to store the estimation calculations
 
+    function resetFields(){
+        temp = [];
+        setWeightState("");
+        setRepState("");
+    }
 
     useEffect(() => {
         let orm = Math.round((weightState * repState * 0.0333) + weightState);
-        let temp = [];
         for(let i = 0; i < 10; ++i){
             temp.push(orm * (100 - 3 * i)/ 100);
         }
         setEstimate(temp)
-    })
+    }, [weightState, repState])
 
     return(
+        // https://stackoverflow.com/questions/52409837/how-can-i-use-a-placeholder-with-a-number-input-in-react
         <div className="calculator-container">
-            <div className="input-container">
-                <div>
-                    <input
-                    type="number"
-                    placeholder="Weight (lbs)"
-                    onChange={(e) => {
-                        setWeightState(Number(e.target.value));
-                    }}/>
+            <h1>DEBUG OUTPUT:    {weightState} {repState}</h1>
+            <h2>One rep max calculator</h2>
+            <div className="calculator-interaction">
+                <div className="input-container">
+                    <div>
+                        <input
+                        type="number"
+                        value={weightState}
+                        placeholder="Weight (in lbs)"
+                        onChange={(e) => {
+                            setWeightState(Number(e.target.value));
+                        }}
+                        />
+                    </div>
+
+                    <div>
+                        <input
+                        type="number"
+                        value={repState}
+                        placeholder="Reps (amount)"
+                        onChange={(e) => {
+                            setRepState(Number(e.target.value));
+                        }}/>
+                    </div>
                 </div>
 
-                <div>
-                    <input
-                    type="number"
-                    placeholder="Reps"
-                    onChange={(e) => {
-                        setRepState(Number(e.target.value));
-                    }}/>
+                <div className="reset-container">
+                    <button className="reset-button" onClick={resetFields}>Reset</button>
                 </div>
             </div>
+
             <div className="calculator-estimate-container">
                 {estimate.map((out, index) => {
-                    if(out > 0){
+                    if( repState > 0 && weightState > 0){
                         return(
                             <div className ="output-container">
                                 <p className="rep-text">
